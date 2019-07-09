c2b5fcdef6eccb7d86e44e3c65fb6787f984a327
김태오 <38588097+Sunday5214@users.noreply.github.com>
2019-07-09 22:19:38 +0900

    Create RelayControl.ino

---
+++ RelayControl/RelayControl.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+
+}
+int a = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+
+//  digitalWrite(5, LOW);
+//  delay(1000);
+//  digitalWrite(5, HIGH);
+//  delay(1000);
+}
